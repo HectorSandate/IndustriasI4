fea3e532483bc1f9f4513c14ddf659d59378b486
Héctor José Díaz Sandate <hectorjosediazsandate@gmail.com>
2024-04-17 02:13:18 -0600

    link

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://industriasi4front.onrender.com",
     // preflightContinue: true,
     credentials: true,
   })
